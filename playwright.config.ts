4fad11464eda86d9f52c34ac9b7ee215cefc89f2
Shantanu Roy <sro@houseofmath.com>
2024-02-14 13:43:03 +0600

    first commit

---
+++ playwright.config.ts
@@ -0,0 +1,41 @@
+import {defineConfig, devices} from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+    testDir: '.',
+    timeout: 60000,
+    expect: {
+        timeout: 5000,
+    },
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 1 : 0,
+    workers: process.env.CI ? 2 : 1,
+    reporter: process.env.CI ? 'github' : 'html',
+    maxFailures: undefined,
+    use: {
+        headless: false,
+        actionTimeout: 10000,
+        navigationTimeout: 30000,
+        trace: 'retain-on-failure',
+        video: 'off',
+        testIdAttribute: 'data-test-id',
+    },
+
+    projects: [
+        {
+            name: 'chromium',
+            use: {...devices['Desktop Chrome']},
+        },
+    ],
+    outputDir: 'test-results/',
+
+});
